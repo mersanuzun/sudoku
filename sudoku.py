a87c36f38445cd7022bec859e2e1cad7709ecff2
mersanuzun <mersanuzun@gmail.com>
2015-11-04 00:22:21 +0200

    printBoard is added and close to solve the sudoku

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,9 @@
+from __future__ import print_function
 from Box import *
 boxes = []
 
 board = [
-    [3,0,0,2,4,0,0,0,0],
+    [3,0,0,2,4,0,0,6,0],
     [0,4,0,0,0,0,0,5,3],
     [1,8,9,6,3,5,4,0,0],
     [0,0,0,0,8,0,2,0,0],
@@ -12,24 +13,34 @@ board = [
     [2,0,0,3,0,0,7,4,0],
     [9,6,0,5,0,0,3,0,2]
 ]
-
-
-
-        
-        
+  
 def generateBoxes():
     for i in range(0,7,3):
         for j in range(0,7,3):
             boxes.append(Box(i, j))
 
+
 def solveSudoku():
+    possibleNumbers = []
+    #while(boxes.length != 0):
     for box in boxes:
-        for i in range(box.startingX, box.startingX + 3):
-            for j in range(box.startingY, box.startingY + 3):
-                if board[i][j] == 0:
-                    for k in range(1,10):
-                        
-                    
+        for number in range(1,10):
+            for i in range(box.startingX, box.startingX + 3):
+                for j in range(box.startingY, box.startingY + 3):
+                    if board[i][j] == 0:
+                        if lookOwnRow(i, number) == False and lookOwnColumn(j, number) == False and lookOwnBox(box.startingX, box.startingY, number) == False:
+                            possibleNumbers.append({"x" : i, "y" : j, "number" : number})
+            if len(possibleNumbers) == 1:
+                board[possibleNumbers[0]["x"]][possibleNumbers[0]["y"]] = possibleNumbers[0]["number"]
+            possibleNumbers = []
+            
+                #box.checkCompleted(board)
+    printBoard()
+def printBoard():
+    for i in range(0, len(board)):
+        for j in range(0, len(board)):
+            print (board[i][j], end=" ")
+        print ()
 def executeOwnColumn(y):
     numbers = [1,2,3,4,5,6,7,8,9]
     for i in range(0,len(board)):
@@ -42,7 +53,7 @@ def executeOwnRow(x):
     numbers = [1,2,3,4,5,6,7,8,9]
     for j in range(0, len(board)):
         if board[x][j] != 0:
-            print board[x][j]
+            print (board[x][j])
             numbers.remove(board[x][j])
     return numbers
         
@@ -66,3 +77,4 @@ def lookOwnColumn(column, number):
     return False
 
 generateBoxes()
+solveSudoku()
